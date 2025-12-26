cbe0540da6407041be3238408dd9ecf743034911
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2025-12-26 15:22:06 +0900

    feat: 테마 저장소 추가

---
+++ src/utils/theme-store.js
@@ -0,0 +1,39 @@
+class ThemeStore {
+	constructor() {
+		this.listeners = [];
+		this.currentTheme = localStorage.getItem("stealth-chat-theme") || "vscode";
+		this.themes = {};
+	}
+
+	// Register a theme object
+	registerTheme(name, themeObject) {
+		this.themes[name] = themeObject;
+	}
+
+	getTheme() {
+		return this.themes[this.currentTheme] || {};
+	}
+
+	setTheme(themeName) {
+		if (this.currentTheme == themeName) return;
+
+		this.currentTheme = themeName;
+		localStorage.setItem("stealth-chat-theme", themeName);
+		this.notify();
+	}
+
+	subscribe(listener) {
+		this.listeners.push(listener);
+		// Return unsubscribe function
+		return () => {
+			this.listeners = this.listeners.filter(l => l != listener);
+		};
+	}
+
+	notify() {
+		this.listeners.forEach(listener => listener(this.getTheme()));
+	}
+}
+
+const themeStore = new ThemeStore();
+export default themeStore;
