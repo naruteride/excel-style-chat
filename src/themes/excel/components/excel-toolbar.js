ab690fae073888e3d53aefa765205d8424f4f278
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2025-12-30 16:48:00 +0900

    feat: 툴바 아이콘 삽입

--- src/themes/excel/components/excel-toolbar.js
+++ src/themes/excel/components/excel-toolbar.js
@@ -13,10 +13,69 @@ class ExcelToolbar extends HTMLElement {
 	render() {
 		this.style.cssText = "display: flex; align-items: center; justify-content: space-between; background-color: #f0f4f9; border-radius: 1.5rem; margin: 6px 16px 8px 16px; min-height: 40px; padding: 0 8px; -webkit-font-smoothing: antialiased;";
 		this.innerHTML = `
-			<div style="display: flex; align-items: center; gap: 0.125rem">
-				
+			<div style="display: flex; align-items: center; gap: 0.125rem; margin-inline: 0.75rem;">
+				<button is="excel-icon-button" image-position-left="-670px" image-position-top="-62px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<button is="excel-icon-button" image-position-left="-680px" image-position-top="-1304px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<button is="excel-icon-button" image-position-left="-236px" image-position-top="-264px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<button is="excel-icon-button" image-position-left="-1168px" image-position-top="-976px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<button is="excel-icon-button" image-position-left="-130px" image-position-top="-152px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<!-- 확대축소 드롭다운 -->
+
+				<hr is="excel-toolbar-hr">
+
+				<button is="excel-toolbar-text-button">
+					₩
+				</button>
+				<button is="excel-toolbar-text-button">
+					%
+				</button>
+				<button is="excel-icon-button" image-position-left="-218px" image-position-top="-68px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<button is="excel-icon-button" image-position-left="-906px" image-position-top="-22px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<button is="excel-toolbar-text-button" style="font-size: 12px;">
+					123
+				</button>
+
+				<hr is="excel-toolbar-hr">
+
+				<!-- 폰트 변경 드롭다운 -->
+
+				<hr is="excel-toolbar-hr">
+
+				<button is="excel-icon-button" image-position-left="-427px" image-position-top="-526px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<!-- 글자 크기 변경 인풋 -->
+				<button is="excel-icon-button" image-position-left="-536px" image-position-top="-394px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+
+				<hr is="excel-toolbar-hr">
+
+				<button is="excel-icon-button" image-position-left="-350px" image-position-top="-106px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<button is="excel-icon-button" image-position-left="-872px" image-position-top="-2060px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<button is="excel-icon-button" image-position-left="-464px" image-position-top="-1500px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<button is="excel-icon-button" image-position-left="-626px" image-position-top="-1938px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+
+				<hr is="excel-toolbar-hr">
+
+				<button is="excel-icon-button" image-position-left="-764px" image-position-top="-330px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<button is="excel-icon-button" image-position-left="-1198px" image-position-top="-1164px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<button is="excel-icon-button" image-position-left="-388px" image-position-top="-898px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<excel-dropdown-icon></excel-dropdown-icon>
+
+				<hr is="excel-toolbar-hr">
+
+				<button is="excel-icon-button" image-position-left="-864px" image-position-top="-808px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<excel-dropdown-icon></excel-dropdown-icon>
+				<button is="excel-icon-button" image-position-left="-1278px" image-position-top="-572px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<excel-dropdown-icon></excel-dropdown-icon>
+				<button is="excel-icon-button" image-position-left="-304px" image-position-top="-284px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<excel-dropdown-icon></excel-dropdown-icon>
+				<button is="excel-icon-button" image-position-left="-376px" image-position-top="-1338px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
+				<excel-dropdown-icon></excel-dropdown-icon>
+
+				<hr is="excel-toolbar-hr">
+
+				<button is="excel-icon-button" image-position-left="-162px" image-position-top="-1878px" style="width: 1.75rem; height: 1.75rem; padding: 0.5rem;"></button>
 			</div>
-			<div style="display: flex; align-items: center; gap: 0.125rem">
+
+			<div style="display: flex; align-items: center; gap: 0.125rem; margin-inline: 0.75rem;">
 				<button is="excel-icon-button" image-position-left="-756px" image-position-top="-1110px" style="width: 1.25rem; height: 1.25rem;"></button>
 			</div>
 		`;
@@ -25,3 +84,34 @@ class ExcelToolbar extends HTMLElement {
 
 customElements.define("excel-toolbar", ExcelToolbar);
 export default ExcelToolbar;
+
+class ExcelToolbarHr extends HTMLHRElement {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		this.style.cssText = "border-color: #c7c7c7; margin-inline: 3px; width: 0; height: 10px;";
+	}
+}
+customElements.define("excel-toolbar-hr", ExcelToolbarHr, { extends: "hr" });
+
+class ExcelToolbarTextButton extends HTMLButtonElement {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		const style = this.getAttribute("style");
+		this.style.cssText = `display: flex; justify-content: center; align-items: center; width: 1.75rem; height: 1.75rem; padding: none; border: none; text-align: center; font-size: 13px; font-weight: 500; ${style}`;
+	}
+}
+customElements.define("excel-toolbar-text-button", ExcelToolbarTextButton, { extends: "button" });
