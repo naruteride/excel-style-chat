3b8b07098fdcb25a3f61e25231754b76f810b2f0
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2025-12-30 14:41:49 +0900

    feat: 엑셀 툴바 컴포넌트

--- src/themes/excel/components/excel-toolbar.js
+++ src/themes/excel/components/excel-toolbar.js
@@ -1,3 +1,6 @@
+import ExcelDropdownButton from "./excel-dropdown-button.js";
+import ExcelIcon, { ExcelIconButton } from "./excel-icon.js";
+
 class ExcelToolbar extends HTMLElement {
 	constructor() {
 		super();
@@ -8,9 +11,14 @@ class ExcelToolbar extends HTMLElement {
 	}
 
 	render() {
-		this.style.cssText = "background-color: #f0f4f9; border-radius: 1.5rem; font-family: Google Sans, Roboto, sans-serif; margin: 6px 16px 8px 16px; min-height: 40px; padding: 0 8px; -webkit-font-smoothing: antialiased;";
+		this.style.cssText = "display: flex; align-items: center; justify-content: space-between; background-color: #f0f4f9; border-radius: 1.5rem; margin: 6px 16px 8px 16px; min-height: 40px; padding: 0 8px; -webkit-font-smoothing: antialiased;";
 		this.innerHTML = `
-			
+			<div style="display: flex; align-items: center; gap: 0.125rem">
+				
+			</div>
+			<div style="display: flex; align-items: center; gap: 0.125rem">
+				<button is="excel-icon-button" image-position-left="-0px" image-position-top="-236px" style="width: 1.125rem; height: 1.125rem;"></button>
+			</div>
 		`;
 	}
 }
