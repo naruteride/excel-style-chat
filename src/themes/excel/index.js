f24a31b24f7a47a74b6b4367f0d15a1cd700733e
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2025-12-31 11:32:27 +0900

    feat: 엑셀에 글로벌 CSS 주입 기능 추가

--- src/themes/excel/index.js
+++ src/themes/excel/index.js
@@ -1,6 +1,15 @@
 import "./excel-login/index.js";
 import "./excel-chat/index.js";
 
+// Inject Excel Theme CSS
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "/src/themes/excel/index.css";
+// Avoid duplicate injection
+if (!document.querySelector(`link[href="${link.href}"]`)) {
+    document.head.appendChild(link);
+}
+
 export default {
     name: "excel",
     // Defines which Custom Element tag to use for each route/view
