5960711024887f8362bd62c570c11b56ffa4d94e
Bang Seonghun <slimejam01@gmail.com>
2026-01-03 20:04:57 +0900

    fix: 모바일 화면에서 헤더가 뷰포트에 스크롤을 만드는 오류 수리

--- src/themes/excel/components/excel-header.js
+++ src/themes/excel/components/excel-header.js
@@ -12,7 +12,7 @@ class ExcelHeader extends HTMLElement {
 	}
 
 	render() {
-		this.style.cssText = "display: flex; flex-direction: column; align-items: stretch; background: #f9fbfd; border-bottom: 1px solid #f9fbfd; margin-bottom: -1px; white-space: nowrap;";
+		this.style.cssText = "display: flex; flex-direction: column; align-items: stretch; background: #f9fbfd; border-bottom: 1px solid #f9fbfd; margin-bottom: -1px; white-space: nowrap; overflow: hidden;";
 		this.innerHTML = `
 			<!-- Menu -->
 			<menu is="excel-menu" room="${this.getAttribute("room")}"></menu>
