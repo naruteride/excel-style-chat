67551a1259d2a0ac4564c614bc08836831a78c14
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2026-01-05 11:51:02 +0900

    feat: 포뮬러바 디자인 고도화

--- src/themes/excel/components/excel-formula-bar.js
+++ src/themes/excel/components/excel-formula-bar.js
@@ -23,6 +23,10 @@ export default class ExcelFormulaBar extends HTMLElement {
 					<excel-icon image-position-left="-746px" image-position-top="-1396px" style="width: 1.125rem; height: 1.125rem;"></excel-icon>
 				</div>
 			</div>
+			<button style="display: flex; justify-content: center; align-items: center; border-radius: 2rem; padding: 0.25rem; background: rgb(240, 244, 249); font-size: 0.813rem; color: rgb(31, 31, 31);">
+				<svg xmlns="http://www.w3.org/2000/svg" height="16px" width="16px" viewBox="0 -960 960 960" fill="none"><path d="M480-80q0-83-31.5-156T363-363q-54-54-127-85.5T80-480q83 0 156-31.5T363-597q54-54 85.5-127T480-880q0 83 31.5 156T597-597q54 54 127 85.5T880-480q-83 0-156 31.5T597-363q-54 54-85.5 127T480-80Z" fill="#1F1F1F"></path></svg>
+				이 데이터 요약
+			</button>
 		`;
 	}
 }
@@ -39,7 +43,7 @@ class ExcelFormulaBarHr extends HTMLHRElement {
 	}
 
 	render() {
-		this.style.cssText = "border: rgb(199, 199, 199) 1px solid; border-right: 0; margin-inline: 3px; width: 0; height: 20px;";
+		this.style.cssText = "border: rgb(199, 199, 199) 1px solid; border-right: 0; margin-block: 0; margin-inline: 3px; width: 0; height: 17px;";
 	}
 }
 customElements.define("excel-formula-bar-hr", ExcelFormulaBarHr, { extends: "hr" });
