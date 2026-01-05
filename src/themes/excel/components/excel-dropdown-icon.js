8de3a703476cee96023a61e5393737bdd678e391
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2026-01-05 13:03:02 +0900

    feat: 엑셀 드롭다운 아이콘 고도화

--- src/themes/excel/components/excel-dropdown-icon.js
+++ src/themes/excel/components/excel-dropdown-icon.js
@@ -8,7 +8,8 @@ export default class ExcelDropdownIcon extends HTMLElement {
 	}
 
 	render() {
-		this.style.cssText = "border-style: solid; border-width: 4px 4px 0 4px; border-color: #444746 transparent; height: 0; width: 0; background: none;";
+		const style = this.getAttribute("style");
+		this.style.cssText = `border-style: solid; border-width: 4px 4px 0 4px; border-color: #444746 transparent; height: 0; width: 0; background: none; ${style}`;
 	}
 }
 customElements.define("excel-dropdown-icon", ExcelDropdownIcon);
