e0f062f81e93584bcdb2ab326e3f1f0d4157354f
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2026-02-12 16:25:05 +0900

    feat: vscode 스타일 수정

--- src/themes/vscode/components/vscode-split-view-container.js
+++ src/themes/vscode/components/vscode-split-view-container.js
@@ -13,24 +13,26 @@ export default class VscodeSplitViewContainer extends HTMLElement {
 	}
 
 	render() {
-		this.classList.add("split-view-container");
-		this.style.cssText = "overflow: hidden;";
+		this.classList.add("monaco-enable-motion", "monaco-workbench", "windows", "web", "chromium", "nopanel", "noauxiliarybar", "file-icons-enabled", "vs-dark", "vscode-theme-defaults-themes-dark_plus-json");
+		this.style.cssText = "width: 100%; height: 100%;";
 		this.innerHTML = `
-			<vscode-split-view-view style="top: 0px; height: 26px;">
-				<vscode-part-banner></vscode-part-banner>
-			</vscode-split-view-view>
+			<div class="split-view-container" style="overflow: hidden;">
+				<vscode-split-view-view style="top: 0px; height: 26px;">
+					<vscode-part-banner></vscode-part-banner>
+				</vscode-split-view-view>
 
-			<vscode-split-view-view style="top: 26px; height: 35px;">
-				<vscode-part-titlebar></vscode-part-titlebar>
-			</vscode-split-view-view>
+				<vscode-split-view-view style="top: 26px; height: 35px;">
+					<vscode-part-titlebar></vscode-part-titlebar>
+				</vscode-split-view-view>
 
-			<vscode-split-view-view style="top: 61px; height: ${window.innerHeight - 83}px;">
-				
-			</vscode-split-view-view>
+				<vscode-split-view-view style="top: 61px; height: ${window.innerHeight - 83}px;">
+					
+				</vscode-split-view-view>
 
-			<vscode-split-view-view style="top: ${window.innerHeight - 22}px; height: 22px;">
-				<footer is="vscode-part-statusbar"></footer>
-			</vscode-split-view-view>
+				<vscode-split-view-view style="top: ${window.innerHeight - 22}px; height: 22px;">
+					<footer is="vscode-part-statusbar"></footer>
+				</vscode-split-view-view>
+			</div>
 		`;
 	}
 }
